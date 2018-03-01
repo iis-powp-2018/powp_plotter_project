35ff6e859c1921607e4ef6e44675e6c4bd6680b0
Klaudia Goczal <klaudia@lodz.pl>
2018-03-01 15:57:46 +0100

    Added test for second pattern

---
+++ src/main/java/edu/iis/powp/events/predefine/SelectTestSecondFigureOptionListener.java
@@ -0,0 +1,18 @@
+package edu.iis.powp.events.predefine;
+
+import edu.iis.client.plottermagic.preset.FiguresJoe;
+import edu.iis.powp.app.Application;
+import edu.iis.powp.app.DriverManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SelectTestSecondFigureOptionListener implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        FiguresJoe.figureScript2(Application.getComponent(DriverManager.class).getCurrentPlotter());
+    }
+}
--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -13,7 +13,8 @@ import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
 import edu.iis.powp.appext.ApplicationWithDrawer;
 import edu.iis.powp.events.predefine.SelectChangeVisibleOptionListener;
-import edu.iis.powp.events.predefine.SelectTestFigureOptionListener;
+import edu.iis.powp.events.predefine.SelectTestFirstFigureOptionListener;
+import edu.iis.powp.events.predefine.SelectTestSecondFigureOptionListener;
 import edu.kis.powp.drawer.panel.DefaultDrawerFrame;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 
@@ -28,9 +29,11 @@ public class TestPlotSoftPatterns
 	 * @param context Application context.
 	 */
 	private static void setupPresetTests(Context context) {
-	    SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener();
+	    SelectTestFirstFigureOptionListener selectTestFirstFigureOptionListener = new SelectTestFirstFigureOptionListener();
+        SelectTestSecondFigureOptionListener selectTestSecondFigureOptionListener = new SelectTestSecondFigureOptionListener();
 		
-		context.addTest("Figure Joe 1", selectTestFigureOptionListener);	        
+		context.addTest("Figure Joe 1", selectTestFirstFigureOptionListener);
+        context.addTest("Figure Joe 2", selectTestSecondFigureOptionListener);
 	}
 
 	/**
@@ -42,7 +45,7 @@ public class TestPlotSoftPatterns
 		IPlotter clientPlotter = new ClientPlotter();
 		context.addDriver("Client Plotter", clientPlotter);
 		Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
-		
+
 		IPlotter plotter = new MyAdapter();
 		context.addDriver("Buggy Simulator", plotter);
 
