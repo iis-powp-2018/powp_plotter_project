d04527b64b2b76be409c0b1a72330c84cba4cac5
Monika <jedrzejekmonika@gmail.com>
2018-03-01 15:44:51 +0100

    Fixed figureScript1 drawing

--- src/main/java/edu/iis/powp/adapter/MyAdapter.java
+++ src/main/java/edu/iis/powp/adapter/MyAdapter.java
@@ -5,38 +5,35 @@ import edu.kis.powp.drawer.panel.DrawPanelController;
 import edu.kis.powp.drawer.shape.ILine;
 import edu.kis.powp.drawer.shape.LineFactory;
 
-
 /**
- * Plotter adapter to drawer with several bugs. 
+ * Plotter adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements IPlotter
-{ 
-	private int startX = 0, startY = 0;
-	
+public class MyAdapter extends DrawPanelController implements IPlotter {
+
+    private int startX = 0, startY = 0;
+
     public MyAdapter() {
-		super();
-	}
-    
-	@Override
-    public void setPosition(int x, int y)
-    {
+        super();
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
         this.startX = x;
         this.startY = y;
     }
 
     @Override
-    public void drawTo(int x, int y)
-    {
+    public void drawTo(int x, int y) {
         ILine line = LineFactory.getBasicLine();
-    	line.setStartCoordinates(this.startX, this.startY);
+        line.setStartCoordinates(this.startX, this.startY);
         line.setEndCoordinates(x, y);
 
-		drawLine(line);
+        drawLine(line);
+        setPosition(x, y);
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "@Q!$!@$!#@$(*#@&Q(%^*#@";
     }
 }
