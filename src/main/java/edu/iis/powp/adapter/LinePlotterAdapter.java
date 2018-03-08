6aac6d643eee5b64b427f27772a8ded1cd35be89
Monika <jedrzejekmonika@gmail.com>
2018-03-08 17:08:15 +0100

    Implemented own LinePlotterAdapter

--- src/main/java/edu/iis/powp/adapter/LinePlotterAdapter.java
+++ src/main/java/edu/iis/powp/adapter/LinePlotterAdapter.java
@@ -3,20 +3,20 @@ package edu.iis.powp.adapter;
 import edu.iis.client.plottermagic.IPlotter;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 import edu.kis.powp.drawer.shape.ILine;
-import edu.kis.powp.drawer.shape.LineFactory;
 
 /**
- * Plotter adapter to drawer with several bugs.
+ * Plotter adapter to LineFactory.
  */
-public class PlotterDrawPanelControllerAdapter implements IPlotter {
+public class LinePlotterAdapter implements IPlotter {
 
     private int startX = 0, startY = 0;
     DrawPanelController drawPanelController;
+    ILine line;
 
-    public PlotterDrawPanelControllerAdapter(DrawPanelController drawPanelController) {
+    public LinePlotterAdapter(DrawPanelController drawPanelController, ILine line) {
         this.drawPanelController = drawPanelController;
+        this.line = line;
     }
-    
 
     @Override
     public void setPosition(int x, int y) {
@@ -26,7 +26,7 @@ public class PlotterDrawPanelControllerAdapter implements IPlotter {
 
     @Override
     public void drawTo(int x, int y) {
-        ILine line = LineFactory.getBasicLine();
+
         line.setStartCoordinates(this.startX, this.startY);
         line.setEndCoordinates(x, y);
 
--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -7,13 +7,14 @@ import java.util.logging.Logger;
 
 import edu.iis.client.plottermagic.ClientPlotter;
 import edu.iis.client.plottermagic.IPlotter;
-import edu.iis.powp.adapter.PlotterDrawPanelControllerAdapter;
+import edu.iis.powp.adapter.LinePlotterAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
 import edu.iis.powp.appext.ApplicationWithDrawer;
 import edu.iis.powp.events.predefine.SelectTestFigureOneOptionListener;
 import edu.iis.powp.events.predefine.SelectTestFigureTwoOptionListener;
+import edu.kis.powp.drawer.shape.LineFactory;
 
 public class TestPlotSoftPatterns {
 
@@ -44,8 +45,17 @@ public class TestPlotSoftPatterns {
         context.addDriver("Client Plotter", clientPlotter);
         Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
 
-        IPlotter plotter = new PlotterDrawPanelControllerAdapter(ApplicationWithDrawer.getDrawPanelController());
-        context.addDriver("Buggy Simulator", plotter);
+        IPlotter basicPlotter = new LinePlotterAdapter(ApplicationWithDrawer.getDrawPanelController(),
+                LineFactory.getBasicLine());
+        context.addDriver("Basic Line Simulator", basicPlotter);
+
+        IPlotter dottedPlotter = new LinePlotterAdapter(ApplicationWithDrawer.getDrawPanelController(),
+                LineFactory.getDottedLine());
+        context.addDriver("Dotted Line Simulator", dottedPlotter);
+
+        IPlotter specialPlotter = new LinePlotterAdapter(ApplicationWithDrawer.getDrawPanelController(),
+                LineFactory.getSpecialLine());
+        context.addDriver("Special Line Simulator", specialPlotter);
 
         context.updateDriverInfo();
     }
