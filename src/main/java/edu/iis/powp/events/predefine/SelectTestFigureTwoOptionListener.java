ae5b691f604df8ef6e4ad467abdff4a85c5d7e5c
Monika <jedrzejekmonika@gmail.com>
2018-03-08 14:47:15 +0100

    Added addTest method for figureScript2

--- src/main/java/edu/iis/powp/events/predefine/SelectTestFigureTwoOptionListener.java
+++ src/main/java/edu/iis/powp/events/predefine/SelectTestFigureTwoOptionListener.java
@@ -7,12 +7,10 @@ import edu.iis.client.plottermagic.preset.FiguresJoe;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.DriverManager;
 
-public class SelectTestFigureOptionListener implements ActionListener
-{
+public class SelectTestFigureTwoOptionListener implements ActionListener {
 
     @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        FiguresJoe.figureScript1(Application.getComponent(DriverManager.class).getCurrentPlotter());
+    public void actionPerformed(ActionEvent e) {
+        FiguresJoe.figureScript2(Application.getComponent(DriverManager.class).getCurrentPlotter());
     }
 }
