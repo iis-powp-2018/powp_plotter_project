f4861278c4d58aa5499159feda61ff26d460fd8f
Patrycja Ignaczak <patrycja.ignaczak@rndity.com>
2018-03-08 15:40:40 +0100

    Removed setupDefaultDrawerVisibilityManagement()
    Now single window view is working properly

--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -48,18 +48,6 @@ public class TestPlotSoftPatterns
 
 		context.updateDriverInfo();
 	}
-
-	/**
-	 * Auxiliary routines to enable using Buggy Simulator.
-	 * 
-	 * @param context Application context.
-	 */
-	private static void setupDefaultDrawerVisibilityManagement(Context context) {
-		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
-        context.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility", 
-        		new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
-        defaultDrawerWindow.setVisible(true);
-	}
 	
 	/**
 	 * Setup menu for adjusting logging settings.
@@ -88,9 +76,7 @@ public class TestPlotSoftPatterns
             {
                 ApplicationWithDrawer.configureApplication();
                 Context context = Application.getComponent(Context.class);
-                
-                setupDefaultDrawerVisibilityManagement(context);
-                
+
             	setupDrivers(context);
             	setupPresetTests(context);
             	setupLogger(context);
