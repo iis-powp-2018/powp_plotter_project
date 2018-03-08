cfee1fd704af74a93c006a0962b88d914fe7bc19
Cezary Witkowski <czarecoo@gmail.com>
2018-03-08 15:34:51 +0100

    fixed showing of image

--- src/main/java/edu/iis/powp/adapter/MyAdapter.java
+++ src/main/java/edu/iis/powp/adapter/MyAdapter.java
@@ -9,14 +9,15 @@ import edu.kis.powp.drawer.shape.LineFactory;
 /**
  * Plotter adapter to drawer with several bugs. 
  */
-public class MyAdapter extends DrawPanelController implements IPlotter
+public class MyAdapter implements IPlotter
 { 
 	private int startX = 0, startY = 0;
+	DrawPanelController drawPanelController;
 	
-    public MyAdapter() {
-		super();
+	public MyAdapter(DrawPanelController drawPanel) {
+		this.drawPanelController = drawPanel;
 	}
-    
+	
 	@Override
     public void setPosition(int x, int y)
     {
@@ -32,7 +33,7 @@ public class MyAdapter extends DrawPanelController implements IPlotter
         line.setEndCoordinates(x, y);
         this.startX=x;
         this.startY=y;
-		drawLine(line);
+        drawPanelController.drawLine(line);
     }
 
     @Override
