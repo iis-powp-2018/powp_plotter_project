ad9c43249ba4c1deef70d6740be3b64d90f54e88
Kamil Krupa <kamilkrupa96@gmail.com>
2018-03-08 15:28:39 +0100

    Fix bug causing drawing problems, refactor adapter name

--- src/main/java/edu/iis/powp/adapter/DrawPanelPlotterAdapter.java
+++ src/main/java/edu/iis/powp/adapter/DrawPanelPlotterAdapter.java
@@ -9,11 +9,11 @@ import edu.kis.powp.drawer.shape.LineFactory;
 /**
  * Plotter adapter to drawer with several bugs. 
  */
-public class MyAdapter extends DrawPanelController implements IPlotter
+public class DrawPanelPlotterAdapter extends DrawPanelController implements IPlotter
 { 
 	private int startX = 0, startY = 0;
 	
-    public MyAdapter() {
+    public DrawPanelPlotterAdapter() {
 		super();
 	}
     
@@ -32,6 +32,9 @@ public class MyAdapter extends DrawPanelController implements IPlotter
         line.setEndCoordinates(x, y);
 
 		drawLine(line);
+		
+		this.startX = x;
+		this.startY = y;
     }
 
     @Override
