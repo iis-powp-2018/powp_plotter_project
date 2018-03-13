268ac900ab86094ff95c358d090900c3884d283c
[Roksana_Traczyk] <[zakrencona15@wp.pl]>
2018-03-13 23:27:07 +0100

    change class name from MyAdapter to DrawPlotterAdapter

--- src/main/java/edu/iis/powp/adapter/DrawPlotterAdapter.java
+++ src/main/java/edu/iis/powp/adapter/DrawPlotterAdapter.java
@@ -10,11 +10,12 @@ import edu.kis.powp.drawer.shape.LineFactory;
 /**
  * Plotter adapter to drawer with several bugs. 
  */
-public class MyAdapter extends DrawPanelController implements IPlotter
+public class DrawPlotterAdapter extends DrawPanelController implements IPlotter
 { 
 	private int startX = 0, startY = 0;
+
 	
-    public MyAdapter() {
+    public DrawPlotterAdapter() {
 		super();
 	}
     
--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -7,7 +7,8 @@ import java.util.logging.Logger;
 
 import edu.iis.client.plottermagic.ClientPlotter;
 import edu.iis.client.plottermagic.IPlotter;
-import edu.iis.powp.adapter.MyAdapter;
+import edu.iis.powp.adapter.DrawPlotterAdapter;
+import edu.iis.powp.adapter.DrawPlotterAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
@@ -30,7 +31,7 @@ public class TestPlotSoftPatterns
 	private static void setupPresetTests(Context context) {
 	    SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener();
 		
-		context.addTest("Figure Joe 1", selectTestFigureOptionListener);	        
+		context.addTest("Figure Joe 1", selectTestFigureOptionListener);
 	}
 
 	/**
@@ -43,7 +44,7 @@ public class TestPlotSoftPatterns
 		context.addDriver("Client Plotter", clientPlotter);
 		Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
 		
-		IPlotter plotter = new MyAdapter();
+		IPlotter plotter = new DrawPlotterAdapter();
 		context.addDriver("Buggy Simulator", plotter);
 
 		context.updateDriverInfo();
