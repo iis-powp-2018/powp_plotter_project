d8bd5b9b8639a5a4c84a5b209dc82089fd56b734
Piotr Sadza <zizi5673@gmail.com>
2018-03-14 21:10:11 +0100

    Changing the name for adapter class

--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 
 import edu.iis.client.plottermagic.ClientPlotter;
 import edu.iis.client.plottermagic.IPlotter;
-import edu.iis.powp.adapter.MyAdapter;
+import edu.iis.powp.adapter.IPlotterAndDrawPanelControllerAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
@@ -44,7 +44,7 @@ public class TestPlotSoftPatterns
 		context.addDriver("Client Plotter", clientPlotter);
 		Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
 
-		IPlotter plotter = new MyAdapter();
+		IPlotter plotter = new IPlotterAndDrawPanelControllerAdapter();
 		context.addDriver("Buggy Simulator", plotter);
 
 		context.updateDriverInfo();
