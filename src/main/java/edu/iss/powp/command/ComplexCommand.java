5b728a746fce489cf70ca5aa80a05b81d409fad1
Berendhard <berendhard@gmail.com>
2018-03-15 15:31:56 +0100

    Updated ComplexCommand class

--- src/main/java/edu/iss/powp/command/ComplexCommand.java
+++ src/main/java/edu/iss/powp/command/ComplexCommand.java
@@ -1,13 +1,21 @@
 package edu.iss.powp.command;
 
+import java.util.List;
+
 import edu.iis.client.plottermagic.IPlotter;
 
 public class ComplexCommand implements PlotterCommand {
+	
+	List<PlotterCommand> command;
+	
+	public ComplexCommand(List<PlotterCommand> command) {
+		this.command = command;
+	}
 
 	@Override
 	public void execute(IPlotter iPlotter) {
-		// TODO Auto-generated method stub
-
+		command.stream().forEach((pc) -> {
+			pc.execute(iPlotter);
+		});
 	}
-
 }
