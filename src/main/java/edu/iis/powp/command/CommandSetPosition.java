3ca9fcd0f384d7435c2c03f3e78513ab71d71d5f
Mrucznik <mrucznix@gmail.com>
2018-03-15 15:03:45 +0100

    Implementacja CommandSetPosition

--- src/main/java/edu/iis/powp/command/CommandSetPosition.java
+++ src/main/java/edu/iis/powp/command/CommandSetPosition.java
@@ -4,10 +4,18 @@ import edu.iis.client.plottermagic.IPlotter;
 
 public class CommandSetPosition implements PlotterCommand {
 
+	private int x;
+	private int y;
+	
+	public CommandSetPosition(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
 	@Override
 	public void execute(IPlotter driver) {
-		// TODO Auto-generated method stub
-		
+		driver.setPosition(x, y);
 	}
 
 }
