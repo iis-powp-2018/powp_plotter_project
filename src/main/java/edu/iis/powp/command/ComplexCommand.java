92065e4adffc21f0eaaa95a47796fe1606bc818d
SwiderskaA <ola@MacBook-Air-Aleksandra.local>
2018-03-15 15:36:04 +0100

    Fixed constructor and addCommand in ComplexCommand

--- src/main/java/edu/iis/powp/command/ComplexCommand.java
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -4,9 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComplexCommand {
-	List<PlotterCommand> listOfCommands=new ArrayList<>();
+	List<PlotterCommand> listOfCommands;
 	
-    void addCommand() {
-    	//needs implementation yet
+    public ComplexCommand() {
+		super();
+		this.listOfCommands = new ArrayList<>();
+	}
+
+
+	void addCommand(PlotterCommand command) {
+    	//needs to fix this implementation yet 
+    	if(command!=null) {
+    	listOfCommands.add(command);
+    	}
 	}
 }
