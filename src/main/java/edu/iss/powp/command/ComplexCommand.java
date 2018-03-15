52d5eb61193aa0dcf72c65f4fd3f07115de0e6c1
Berendhard <berendhard@gmail.com>
2018-03-15 15:30:07 +0100

    pkt 3.1.2 Add ComplexCommand class
    pkt 3.1.2

---
+++ src/main/java/edu/iss/powp/command/ComplexCommand.java
@@ -0,0 +1,13 @@
+package edu.iss.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class ComplexCommand implements PlotterCommand {
+
+	@Override
+	public void execute(IPlotter iPlotter) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
