12926c11fc18b2edd14d2543d6c0ee487f972fe4
Mrucznik <mrucznix@gmail.com>
2018-03-15 15:02:40 +0100

    Implementacja CommandDrawLineToPosition

--- src/main/java/edu/iis/powp/command/CommandDrawLineToPosition.java
+++ src/main/java/edu/iis/powp/command/CommandDrawLineToPosition.java
@@ -4,9 +4,18 @@ import edu.iis.client.plottermagic.IPlotter;
 
 public class CommandDrawLineToPosition implements PlotterCommand {
 
+	private int x;
+	private int y;
+	
+	public CommandDrawLineToPosition(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
 	@Override
 	public void execute(IPlotter driver) {
-		
+		driver.drawTo(x, y);
 	}
 
 }
