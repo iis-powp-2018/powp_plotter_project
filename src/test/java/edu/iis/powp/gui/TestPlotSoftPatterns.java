692a785e2ae006a3172f8b1678777f5383a3fbbf
Klaudia Goczal <klaudia@lodz.pl>
2018-03-15 15:34:19 +0100

    Added CommandComplex and test for it

--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -12,10 +12,7 @@ import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
 import edu.iis.powp.appext.ApplicationWithDrawer;
-import edu.iis.powp.events.predefine.SelectCommandTestListener;
-import edu.iis.powp.events.predefine.SelectJaneTestFigureOptionListener;
-import edu.iis.powp.events.predefine.SelectJoeTestFirstFigureOptionListener;
-import edu.iis.powp.events.predefine.SelectJoeTestSecondFigureOptionListener;
+import edu.iis.powp.events.predefine.*;
 import edu.kis.powp.drawer.shape.LineFactory;
 
 public class TestPlotSoftPatterns
@@ -32,11 +29,13 @@ public class TestPlotSoftPatterns
         SelectJoeTestSecondFigureOptionListener selectJoeTestSecondFigureOptionListener = new SelectJoeTestSecondFigureOptionListener();
 		SelectJaneTestFigureOptionListener selectJaneTestFigureOptionListener = new SelectJaneTestFigureOptionListener();
 		SelectCommandTestListener selectCommandTestListener = new SelectCommandTestListener();
+        SelectCommandComplexTestListener selectCommandComplexTestListener = new SelectCommandComplexTestListener();
 		
 		context.addTest("Figure Joe 1", selectJoeTestFirstFigureOptionListener);
         context.addTest("Figure Joe 2", selectJoeTestSecondFigureOptionListener);
 		context.addTest("Figure Jane", selectJaneTestFigureOptionListener);
-		context.addTest("Command Test", selectCommandTestListener);
+		context.addTest("Command Simple Test", selectCommandTestListener);
+        context.addTest("Command Complex Test", selectCommandComplexTestListener);
 	}
 
 	/**
