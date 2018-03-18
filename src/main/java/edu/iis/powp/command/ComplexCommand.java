58203e5517a78a4cdc1e33c015c9d65505ce33ae
SwiderskaA <ola@macbook-air-aleksandra.home>
2018-03-18 21:34:33 +0100

    added complex command

--- src/main/java/edu/iis/powp/command/ComplexCommand.java
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -3,19 +3,28 @@ package edu.iis.powp.command;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.iis.client.plottermagic.IPlotter;
+
 public class ComplexCommand {
-	List<PlotterCommand> listOfCommands;
 	
-    public ComplexCommand() {
-		super();
+	private final IPlotter plotter;
+	private List<PlotterCommand> listOfCommands;
+
+	public ComplexCommand(IPlotter plotter) {
+		
+		this.plotter = plotter;
 		this.listOfCommands = new ArrayList<>();
 	}
-
+	
+	
 
 	void addCommand(PlotterCommand command) {
-    	//needs to fix this implementation yet 
-    	if(command!=null) {
-    	listOfCommands.add(command);
-    	}
+		if (command != null) {
+			listOfCommands.add(command);
+		}
+	}
+	
+	public void performAll() {
+		listOfCommands.stream().forEach(command -> command.execute(plotter));
 	}
 }
