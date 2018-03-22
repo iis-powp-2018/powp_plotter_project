2f3558de64869852ba09e76c15c4046634e3c3cb
Jacek Olszewski <202256@edu.p.lodz.pl>
2018-03-22 15:20:36 +0100

    Dodanie funkcjonalności klasy ComplexCommand.

--- src/main/java/edu/iis/powp/command/ComplexCommand.java
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -1,13 +1,22 @@
 package edu.iis.powp.command;
 
+import java.util.ArrayList;
+
 import edu.iis.client.plottermagic.IPlotter;
 
 public class ComplexCommand implements PlotterCommand {
-
+	
+	ArrayList<PlotterCommand> commandList  = new ArrayList<PlotterCommand>();
+	
 	@Override
 	public void execute(IPlotter driver) {
-		// TODO Auto-generated method stub
-
+		for(PlotterCommand command : commandList) {
+			command.execute(driver);
+		}
 	}
-
+	
+	public void setCommandList(ArrayList<PlotterCommand> commandListArg) {
+		commandList.clear();
+		commandList.addAll(commandListArg);
+	}
 }
