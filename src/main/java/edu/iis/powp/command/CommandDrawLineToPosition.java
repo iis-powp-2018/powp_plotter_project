ac9714d89a10704f45457dea0d5607759b91d73a
Artur Sonenberg <195060@edu.p.lodz.pl>
2018-03-22 15:02:07 +0100

    Initialization of two classes related to PlotterCommand interface

---
+++ src/main/java/edu/iis/powp/command/CommandDrawLineToPosition.java
@@ -0,0 +1,14 @@
+package edu.iis.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class CommandDrawLineToPosition implements PlotterCommand
+{
+	private int x, y;
+	@Override
+	public void execute(IPlotter driver) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
