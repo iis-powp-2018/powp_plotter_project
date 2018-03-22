c969212b293afd815bb82d56965a55ff146874b6
Jacek Olszewski <202256@edu.p.lodz.pl>
2018-03-22 15:05:33 +0100

    Dodanie funkcjonalności drawTo i setPosition do klas Command.

--- src/main/java/edu/iis/powp/command/CommandSetPosition.java
+++ src/main/java/edu/iis/powp/command/CommandSetPosition.java
@@ -1,12 +1,31 @@
 package edu.iis.powp.command;
 
+import edu.iis.client.plottermagic.IPlotter;
+
 public class CommandSetPosition implements PlotterCommand{
 
 	private int x;
 	private int y;
-
 	@Override
-	public void execute() {
-		
+	public void execute(IPlotter driver) {
+		driver.setPosition(x, y);
 	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
 }
--- src/main/java/edu/iis/powp/command/PlotterCommand.java
+++ src/main/java/edu/iis/powp/command/PlotterCommand.java
@@ -3,5 +3,5 @@ package edu.iis.powp.command;
 import edu.iis.client.plottermagic.IPlotter;
 
 public interface PlotterCommand{
-	public void execute();
+	public void execute(IPlotter driver);
 }
