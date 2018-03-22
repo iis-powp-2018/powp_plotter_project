0294e37be17266b17682b537689b4767bdd30e8c
Patrycja Ignaczak <patrycja.ignaczak@rndity.com>
2018-03-22 15:35:40 +0100

    command: add CompleCommand interface

--- src/main/java/edu/iis/powp/command/CommandSetPosition.java
+++ src/main/java/edu/iis/powp/command/CommandSetPosition.java
@@ -1,5 +1,22 @@
 package edu.iis.powp.command;
 
-public class CommandSetPosition {
+import edu.iis.client.plottermagic.IPlotter;
+
+public class CommandSetPosition implements IPlotterCommand{
+
+    private int posX;
+    private int posY;
+
+    @Override
+    public void execute(IPlotter iPlotter) {
+        iPlotter.setPosition(posX, posY);
+    }
+
+    public CommandSetPosition(int posX, int posY){
+        super();
+        this.posX = posX;
+        this.posY = posY;
+    }
+
 
 }
