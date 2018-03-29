b6f14988e2e56f0f71312c468e6f1fc2cea39709
Piotr Sadza <zizi5673@gmail.com>
2018-03-29 23:30:38 +0200

    Redefining class ComplexCommand

--- src/test/java/edu/iis/powp/command/ComplexCommand.java
+++ src/test/java/edu/iis/powp/command/ComplexCommand.java
@@ -1,25 +1,22 @@
 package edu.iis.powp.command;
 
+import java.util.List;
+
 import edu.iis.client.plottermagic.IPlotter;
 
 public class ComplexCommand implements PlotterCommand {
-	
-	private int x0;
-	private int y0;
-	private int x1;
-	private int y1;
-	
-	public ComplexCommand(int x0, int y0, int x1, int y1) {
-		this.x0=x0;
-		this.y0=y0;
-		this.x1=x1;
-		this.y1=y1;
+
+	private List<PlotterCommand> commandList;
+
+	public ComplexCommand(List<PlotterCommand> commandList) {
+		this.commandList = commandList;
 	}
-	
+
 	@Override
 	public void execute(IPlotter iPlotter) {
-		iPlotter.setPosition(x0, y0);
-		iPlotter.drawTo(x1, y1);
+		for(PlotterCommand command : commandList){
+			command.execute(iPlotter);
+		}
 	}
 
 }
