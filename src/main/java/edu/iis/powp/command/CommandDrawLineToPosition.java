8b34826e98f5d2473b500506a5342c0fdbe136f5
Parada <paradowskim94@gmail.com>
2018-04-05 13:55:46 +0200

    Modified Command Draw Line

--- src/main/java/edu/iis/powp/command/CommandDrawLineToPosition.java
+++ src/main/java/edu/iis/powp/command/CommandDrawLineToPosition.java
@@ -1,5 +1,18 @@
 package edu.iis.powp.command;
 
-public class CommandDrawLineToPosition {
+import edu.iis.client.plottermagic.IPlotter;
 
+public class CommandDrawLineToPosition implements PlotterCommand{
+
+    private int x;
+    private int y;
+
+    @Override public void execute(IPlotter iPlotter) {
+        iPlotter.drawTo(x,y);
+    }
+
+    public CommandDrawLineToPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
