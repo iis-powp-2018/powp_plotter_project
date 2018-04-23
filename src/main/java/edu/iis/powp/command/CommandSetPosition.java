6509bddb62925276e8b3c458117d552d85892bdb
Marcin Dzikowski <dzikum@gmail.com>
2018-04-23 18:44:00 +0200

    started command task, added 3 classes

---
+++ src/main/java/edu/iis/powp/command/CommandSetPosition.java
@@ -0,0 +1,20 @@
+package edu.iis.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class CommandSetPosition implements PlotterCommand {
+
+    private int x;
+    private int y;
+
+    @Override
+    public void execute(IPlotter iPlotter) {
+        iPlotter.setPosition(x, y);
+    }
+
+    public CommandSetPosition(int x, int y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+}
