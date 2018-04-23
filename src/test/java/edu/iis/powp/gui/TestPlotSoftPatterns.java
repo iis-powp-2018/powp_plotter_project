a5b244066ef149561c786137a182e3bc67f5277a
Marcin Dzikowski <dzikum@gmail.com>
2018-04-23 18:18:39 +0200

    changes in TestPlotSoftPatterns

--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 
 import edu.iis.client.plottermagic.ClientPlotter;
 import edu.iis.client.plottermagic.IPlotter;
-
+import edu.iis.powp.adapter.LinePlotterAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
@@ -17,7 +17,7 @@ import edu.iis.powp.events.predefine.SelectTestFigureOptionListener;
 import edu.kis.powp.drawer.panel.DefaultDrawerFrame;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 import edu.iis.powp.adapter.DrawerPlotterAdapter;
-
+import edu.kis.powp.drawer.shape.LineFactory;
 public class TestPlotSoftPatterns
 {
 	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
@@ -44,7 +44,11 @@ public class TestPlotSoftPatterns
 		Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
 		
 		IPlotter plotter = new DrawerPlotterAdapter(ApplicationWithDrawer.getDrawPanelController());
-		context.addDriver("Buggy Simulator", plotter);
+		context.addDriver("Simulator", plotter);
+		
+				LinePlotterAdapter linePlotter = new LinePlotterAdapter(ApplicationWithDrawer.getDrawPanelController());
+				linePlotter.setLine(LineFactory.getDottedLine());
+				context.addDriver("LinePlotter",linePlotter);
 
 		context.updateDriverInfo();
 	}
