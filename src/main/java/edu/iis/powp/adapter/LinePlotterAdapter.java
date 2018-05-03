423e613d0cbd3badba17220fcfcf3e70050ae811
ZarembaKrzysztof <32525870+ZarembaKrzysztof@users.noreply.github.com>
2018-05-03 18:53:18 +0200

    Update LinePlotterAdapter.java

--- src/main/java/edu/iis/powp/adapter/LinePlotterAdapter.java
+++ src/main/java/edu/iis/powp/adapter/LinePlotterAdapter.java
@@ -1,7 +1,6 @@
 package edu.iis.powp.adapter;
 
 import edu.iis.client.plottermagic.IPlotter;
-import edu.kis.powp.drawer.shape.ILine;
 import edu.kis.powp.drawer.shape.LineFactory;
 
 public class LinePlotterAdapter extends PlotterDefaultAdapter implements IPlotter
